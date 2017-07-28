6f26c22a9501770469173a9f583d6cdc18c74731
Paweł Sawicki <psawicki5@gmail.com>
2017-07-28 20:20:10 +0200

    change plot style

--- NewBet/static/betapp/js/plot-standings.js
+++ NewBet/static/betapp/js/plot-standings.js
@@ -30,8 +30,38 @@ $(document).ready(function(){
             y: json['standing_list'],
             type: 'scatter'
         };
+        var layout = {
+            xaxis: {
+                title: 'MATCHDAY',
+                autotick: false,
+                ticks: 'outside',
+                tick0: 0,
+                dtick: 1,
+                ticklen: 8,
+                tickwidth: 2,
+                tickcolor: '#000'
+            },
+            yaxis: {
+                title: 'STANDING',
+                range: [20, 0.5],
+                autotick: false,
+                ticks: 'outside',
+                tick0: 0,
+                dtick: 1,
+                ticklen: 8,
+                tickwidth: 2,
+                tickcolor: '#000'
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                b: 100,
+                t: 100,
+                pad: 4
+            }
+        };
         var data = [trace];
-        Plotly.newPlot('team-standings', data)
+        Plotly.newPlot('team-standings', data, layout)
     };
     ajax_function(competition_id, team_id)
 
